d0a7990eb5cbd9a1da4ce63aff6a61bc2ea04ef0
ac88 <mmaxpokerr@gmail.com>
2021-06-02 13:33:18 +0300

    Updates

---
+++ precache-manifest.31a045eaac08cb5ae8f8138a3e4b0a56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de35c66eefa5d5b0d78c",
+    "url": "/movies-project/static/css/main.4ee59b42.chunk.css"
+  },
+  {
+    "revision": "de35c66eefa5d5b0d78c",
+    "url": "/movies-project/static/js/main.7e29ac3a.chunk.js"
+  },
+  {
+    "revision": "5c508d99150f814ab3b6",
+    "url": "/movies-project/static/js/runtime~main.54b4a2ee.js"
+  },
+  {
+    "revision": "cf1c5f2e89b6c219135c",
+    "url": "/movies-project/static/js/2.d41dfb60.chunk.js"
+  },
+  {
+    "revision": "393a1ff74d7856be56ec53404ce4bc1c",
+    "url": "/movies-project/index.html"
+  }
+];
